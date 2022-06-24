7ccc487f1e2d929e46a020d8ba856d6351fff4e5
osamaalghassani <osamaalghassin@gmail.com>
2022-06-24 21:02:08 +0300

    new

---
+++ card/src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+
+function App() {
+  return (
+    <div classNameName="App">
+      <div className="box">
+<div className="info">
+    <div className="top">
+
+
+    <h1>Get <span>insights</span> that help your bysiness grow.</h1>
+<p className="des">Discover the benefits data analytics and maje beeter descisions regarding revenue, customer experience, and overall efficiency.</p>
+<div className="anlyustec">
+    <div>
+        <h1>10K+</h1>
+        <p>companies</p>
+    </div>
+    <div >
+        <h1>314</h1>
+        <p>Templates</p>
+    </div>
+    <div >
+        <h1>12M+</h1>
+        <p>queries</p>
+    </div>
+</div>
+</div>
+
+</div>
+<div className="img">
+    <div className="overlay"></div>
+</div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
